62248dadc1b89471ddbf9db2d50d45131fbca675
danbleandro <40213277+danbleandro@users.noreply.github.com>
2018-06-25 10:32:17 -0600

    Base de datos CAHUITA

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+Use CAHUITA
+Go
+Create Table REGISTRO(
+id int primary key not null,
+nombre varchar(20),
+apellidos varchar(40),
+fecha_De_Nacimiento datetime,
+correo varchar(20),
+estado varchar(20),
+);
+
+
+
