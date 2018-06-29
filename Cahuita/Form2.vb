e8e5438c284bf004610d93da27f0bd80f641e778
isaac murillo <isaacmurillovega@gmail.com>
2018-06-29 15:27:53 -0600

    Trabaje Registro

---
+++ Cahuita/Form2.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form2
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnRegistrarse.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+
+    End Sub
+End Class
