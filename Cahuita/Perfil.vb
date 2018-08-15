a34132dffda091fffed5fec119c6c4f9b18ac229
Deivid Ugarte <deividauq93@hotmail.com>
2018-08-15 15:43:54 -0700

    mi perfil listo con los datos de la base de datos

--- Cahuita/Perfil.vb
+++ Cahuita/Perfil.vb
@@ -1,17 +1,32 @@
 ﻿Public Class Perfil
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles lblID.Click
-
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
-
-    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles lblApellidosDatos.Click
-
-    End Sub
 
     Private Sub btnRegresarAlMenuPrincipal_Click(sender As Object, e As EventArgs) Handles btnRegresarAlMenuPrincipal.Click
         Me.Close()
     End Sub
+
+    Private Sub Perfil_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cargarDatos()
+    End Sub
+
+    Private Function cargarDatos()
+        conexion.ConnectionString = servidor_datos
+        conexion.Open()
+        Dim instruccionSQL As String
+        instruccionSQL = "Select *  from Empleados where correo ='1?'"
+        instruccionSQL = instruccionSQL.Replace("1?", login.usuario)
+
+        Dim datos As New SqlClient.SqlCommand(instruccionSQL, conexion)
+        Dim datosUser As SqlClient.SqlDataReader
+        datosUser = datos.ExecuteReader()
+        datosUser.Read()
+        lblCorreoDatos.Text = datosUser("correo")
+        lblApellidosDatos.Text = datosUser("apellido")
+        lblFechaDeNacimientoDatos.Text = datosUser("fecha_Nacimiento")
+        lblNombreDatos.Text = datosUser("nombre")
+        lblIDDatos.Text = datosUser("id")
+        lblDireccionDatos.Text = datosUser("direccion")
+        conexion.Close()
+    End Function
+
+
 End Class
